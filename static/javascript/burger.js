9a4342a3ad475a7735a1667811d3f3b9964bfd2a
Chris Mann <chris@lesgrandsvoisins.com>
2023-07-23 18:43:19 +0200

    G Pas

--- static/javascript/burger.js
+++ static/javascript/burger.js
@@ -18,3 +18,61 @@ if (document.getElementById('menu-checkbox')) {
   }
 }
 
+
+
+let toggle = document.querySelector('#jour-nuit');
+
+function lesgvGoDark(toggle) {
+  localStorage.removeItem('lightmode');
+  localStorage.setItem('darkmode', true);
+  toggle.innerText = 'Nuit';
+  document.body.classList.add('darkmode');
+}
+
+function lesgvGoLight(toggle) {
+  localStorage.removeItem('darkmode');
+  localStorage.setItem('lightmode', true);
+  toggle.innerText = 'Jour';
+  document.body.classList.remove('darkmode');
+}
+
+function toggleDarkmode() {
+  let toggle = document.querySelector('#jour-nuit');
+  if (document.body.classList.contains('darkmode')) {
+    lesgvGoLight(toggle);
+  } else {
+    lesgvGoDark(toggle);
+  }
+}
+
+toggle.addEventListener('click', function(e) {
+  if (document.body.classList.contains('darkmode')) {
+    lesgvGoLight(toggle);
+  } else {
+    lesgvGoDark(toggle);
+  }
+});
+
+// Turn the theme off if the 'darkmode' key exists in localStorage
+if (localStorage.getItem('darkmode')) {
+  lesgvGoDark(toggle);
+}  else if (localStorage.getItem('lightmode')) {
+  lesgvGoLight(toggle);
+} else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  lesgvGoDark(toggle);
+}
+
+
+window.addEventListener(
+  "pagehide",
+  (event) => {
+    if (toggle) {
+      if (document.body.classList.contains('darkmode')) {
+        lesgvGoDark(toggle);
+      } else {
+        lesgvGoLight(toggle);
+      }
+    }
+  },
+  false,
+);
