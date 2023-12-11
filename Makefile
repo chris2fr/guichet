209eef9bb7b5e54501b368b7ea042b5b9e9022da
Chris Mann <chris@lesgrandsvoisins.com>
2023-12-11 23:05:26 +0100

    Progression

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 BIN=guichet
-SRC=models/model.go models/user.go models/passwd.go views/admin.go views/home.go views/invite.go views/login.go views/passwd.go views/user.go views/http.go models/ldap.go models/config.go views/directory.go  views/picture.go views/session.go utils/ssha.go models/modelutils.go views/view.go controllers/controller.go models/authentik.go main.go
+SRC=models/model.go models/user.go models/passwd.go views/admin.go views/home.go views/invite.go views/login.go views/passwd.go views/user.go views/http.go models/ldap.go models/config.go views/directory.go  views/picture.go views/session.go utils/ssha.go models/modelutils.go views/view.go controllers/controller.go models/authentik.go openapi/openapi.go main.go
 
 #SRC=models/model.go models/user.go models/passwd.go views/admin.go views/home.go views/invite.go views/login.go views/passwd.go views/user.go utils/http.go utils/ldap.go utils/config.go  utils/ssha.go utils/lesutils.go views/view.go controller.go main.go
 
--- models/authentik.go
+++ models/authentik.go
@@ -3,112 +3,133 @@ package models
 import (
 	"context"
 	"fmt"
-	"net/http"
+	// "net/http"
 	"os"
 
-	httptransport "github.com/go-openapi/runtime/client"
-	api "goauthentik.io/api/v3"
+	// httptransport "github.com/go-openapi/runtime/client"
+	// api "goauthentik.io/api/v3"
+	"guichet/openapi"
+
 )
 
 
-func GetTLSTransport(insecure bool) http.RoundTripper {
-	tlsTransport, err := httptransport.TLSTransport(httptransport.TLSClientOptions{
-		// InsecureSkipVerify: insecure,
-	})
-	if err != nil {
-		panic(err)
-	}
-	return tlsTransport
-}
+// func GetTLSTransport(insecure bool) http.RoundTripper {
+// 	tlsTransport, err := httptransport.TLSTransport(httptransport.TLSClientOptions{
+// 		// InsecureSkipVerify: insecure,
+// 	})
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	return tlsTransport
+// }
 
 
 func SyncAuthentikLDAP () error {
-	// os.Setenv("HTTP_PROXY", "https://auth.lesgrandsvoisins.com")
 
-	authConfig := api.NewConfiguration()
-	authConfig.Debug = true
-	authConfig.Scheme = "http"
-	// authConfig.Host = "auth.lesgrandsvoisins.com"
-	authConfig.Host = "10.245.101.35:9000"
-	authConfig.HTTPClient = &http.Client{}
-	// authConfig.HTTPClient = &http.Client{
-	// 	Transport: GetTLSTransport(true),
-	// }
+	slug := "des-grands-voisins"
+	patchedLDAPSourceRequest := *openapi.NewPatchedLDAPSourceRequest() // PatchedLDAPSourceRequest |  (optional)
+	configuration := openapi.NewConfiguration()
+  apiClient := openapi.NewAPIClient(configuration)
+  resp, r, err := apiClient.SourcesApi.SourcesLdapPartialUpdate(context.Background(), slug).PatchedLDAPSourceRequest(patchedLDAPSourceRequest).Execute()
+  if err != nil {
+      fmt.Fprintf(os.Stderr, "Error when calling `SourcesApi.SourcesLdapPartialUpdate``: %v\n", err)
+      fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+  }
+  // response from `SourcesLdapPartialUpdate`: LDAPSource
+  fmt.Fprintf(os.Stdout, "Response from `SourcesApi.SourcesLdapPartialUpdate`: %v\n", resp)
 
-	authConfig.AddDefaultHeader("Authorization", fmt.Sprintf("Bearer %s", config.AuthentikAPIBearerToken)) 
+
+
+// 	// os.Setenv("HTTP_PROXY", "https://auth.lesgrandsvoisins.com")
+
+// 	authConfig := api.NewConfiguration()
+// 	authConfig.Debug = true
+// 	authConfig.Scheme = "http"
+// 	// authConfig.Host = "auth.lesgrandsvoisins.com"
+// 	authConfig.Host = "10.245.101.35:9000"
+// 	authConfig.HTTPClient = &http.Client{}
+// 	// authConfig.HTTPClient = &http.Client{
+// 	// 	Transport: GetTLSTransport(true),
+// 	// }
+
+// 	authConfig.AddDefaultHeader("Authorization", fmt.Sprintf("Bearer %s", config.AuthentikAPIBearerToken)) 
 	
-  apiClient := api.NewAPIClient(authConfig)
+//   apiClient := api.NewAPIClient(authConfig)
 
-	// return nil
-	// resp, r, err := apiClient.AdminApi.AdminAppsList(context.Background()).Execute()
-	// ctx := context.Background()
-	ldapsrcreq := api.LDAPSourceRequest{
-		Slug: "des-grands-voisins",
-		Name: "Des Grands Voisins",
-		BaseDn: "dc=resdigita,dc=org",
-		ServerUri: "ldap://mail.lesgrandsvoisins.com",
-	}
-	ldapsrcreq.SetSyncUsers(true)
-	ldapsrcsupreq := api.ApiSourcesLdapUpdateRequest{
-		ApiService: apiClient.SourcesApi,
-	}
-	// api.LDAPSourceRequest{
-	// 	Slug: "des-grands-voisins",
-	// }
-	req := api.ApiSourcesLdapUpdateRequest.LDAPSourceRequest(ldapsrcsupreq, ldapsrcreq)
+// 	// return nil
+// 	// resp, r, err := apiClient.AdminApi.AdminAppsList(context.Background()).Execute()
+// 	// ctx := context.Background()
 
-	resp, r, err := req.Execute()
+// 	// appreq	:= api.NewLDAPSyncStatus(true, )
+// 	// apiClient.SourcesApi.SourcesLdapSyncStatusRetrieveExecute()
 
-	// Request(ldapsrcreq)
+// 	ldapsrcreq := api.LDAPSourceRequest{
+// 		Slug: "des-grands-voisins",
+// 		Name: "Des Grands Voisins",
+// 		BaseDn: "dc=resdigita,dc=org",
+// 		ServerUri: "ldap://mail.lesgrandsvoisins.com",
+// 	}
+// 	ldapsrcreq.SetSyncUsers(true)
+// 	ldapsrcsupreq := api.ApiSourcesLdapUpdateRequest{
+// 		ApiService: apiClient.SourcesApi,
+// 	}
+// 	// api.LDAPSourceRequest{
+// 	// 	Slug: "des-grands-voisins",
+// 	// }
+// 	req := api.ApiSourcesLdapUpdateRequest.LDAPSourceRequest(ldapsrcsupreq, ldapsrcreq)
+
+// 	resp, r, err := req.Execute()
+
+// 	// Request(ldapsrcreq)
 
 
 	
  
-	// resp, r, err := apiClient.SourcesApi.SourcesLdapUpdateExecute(req)
+// 	// resp, r, err := apiClient.SourcesApi.SourcesLdapUpdateExecute(req)
 
 
-	// resp, r, err := apiClient.SourcesApi.SourcesLdapUpdate(context.WithValue(context.Background(),))
+// 	// resp, r, err := apiClient.SourcesApi.SourcesLdapUpdate(context.WithValue(context.Background(),))
 	
-	apiClient.SourcesApi.SourcesLdapUpdate(context.Background(),"des-grands-voisins").Execute()
-	// .AdminApi.AdminAppsList(context.Background()).Execute()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error when calling `AdminApi.AdminAppsList``: %v\n", err)
-		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
-		// return err
-	}
-	// response from `AdminAppsList`: []App
-	fmt.Fprintf(os.Stdout, "Response from `AdminApi.AdminAppsList`: %v\n", resp)
+// 	apiClient.SourcesApi.SourcesLdapUpdate(context.Background(),"des-grands-voisins").Execute()
+// 	// .AdminApi.AdminAppsList(context.Background()).Execute()
+// 	if err != nil {
+// 		fmt.Fprintf(os.Stderr, "Error when calling `AdminApi.AdminAppsList``: %v\n", err)
+// 		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+// 		// return err
+// 	}
+// 	// response from `AdminAppsList`: []App
+// 	fmt.Fprintf(os.Stdout, "Response from `AdminApi.AdminAppsList`: %v\n", resp)
 
-	// PATCH /sources/ldap/des-grands-voisins
-// 	curl -X PATCH "https://auth.lesgrandsvoisins.com/api/v3/sources/ldap/des-grands-voisins/" \
-//  -H "accept: application/json"\
-//  -H "content-type: application/json" \
-//  -H "content-type: application/json" \
-//  -d '{}' 
+// 	// PATCH /sources/ldap/des-grands-voisins
+// // 	curl -X PATCH "https://auth.lesgrandsvoisins.com/api/v3/sources/ldap/des-grands-voisins/" \
+// //  -H "accept: application/json"\
+// //  -H "content-type: application/json" \
+// //  -H "content-type: application/json" \
+// //  -d '{}' 
 
-	// // Replace these with your Authentik server details
-	// authURL := "https://auth.lesgrandsvoisins.com/api/v3"
-	// username := "apiuser"
-	// password := "e5MZCP7mH2h5JTqA"
+// 	// // Replace these with your Authentik server details
+// 	// authURL := "https://auth.lesgrandsvoisins.com/api/v3"
+// 	// username := "apiuser"
+// 	// password := "e5MZCP7mH2h5JTqA"
 
-	// // Create a new Authentik client
-	// client := authentik.NewClient(authURL, nil)
+// 	// // Create a new Authentik client
+// 	// client := authentik.NewClient(authURL, nil)
 
-	// // Login to the Authentik server
-	// err := client.Login(username, password)
-	// if err != nil {
-	// 	log.Fatal("Error logging in:", err)
-	// 	return err
-	// }
+// 	// // Login to the Authentik server
+// 	// err := client.Login(username, password)
+// 	// if err != nil {
+// 	// 	log.Fatal("Error logging in:", err)
+// 	// 	return err
+// 	// }
 
-	// // Trigger synchronization for the LDAP user source with slug "des-grands-voisins"
-	// slug := "des-grands-voisins"
-	// err = client.Providers.LDAP.Update(slug)
-	// if err != nil {
-	// 	log.Fatal("Error triggering synchronization:", err)
-	// 	return err
-	// }
+// 	// // Trigger synchronization for the LDAP user source with slug "des-grands-voisins"
+// 	// slug := "des-grands-voisins"
+// 	// err = client.Providers.LDAP.Update(slug)
+// 	// if err != nil {
+// 	// 	log.Fatal("Error triggering synchronization:", err)
+// 	// 	return err
+// 	// }
 
-	// fmt.Println("Synchronization triggered successfully for slug:", slug)
-	return nil
+// 	// fmt.Println("Synchronization triggered successfully for slug:", slug)
+	// return nil
 }
